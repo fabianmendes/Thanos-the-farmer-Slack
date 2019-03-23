98d787af96f6162bea1c05c2fed190e046d31b4d
V. Fabian T. Mendes C <31230743+fabianmendes@users.noreply.github.com>
2019-03-23 05:58:56 +0000

    Upgrade main.py
    Update, recoded. 1st & 2nd Harvest done, also listening and users who'll participate!

--- THANOS/main.py
+++ THANOS/main.py
@@ -1,6 +1,7 @@
 from slackclient import SlackClient
 from slackeventapi import SlackEventAdapter
 import schedule
+import time
 import os
 
 BOT_NAME = "thanos"
@@ -8,7 +9,7 @@ slack_token = os.environ["SLACK_SIGNING_SECRET"] # I'll do it w/ secret signing.
 sc = SlackClient(slack_token)
 
 sea = SlackEventAdapter(slack_token, endpoint="/slack/events")
-										#  SLACK_SIGNING_SECRET,
+			# ^ SLACK_SIGNING_SECRET,
 										
 @sea.on("hear")
 def handle_message(event_data):
@@ -25,17 +26,18 @@ def say(something, towho = False):
 	channel = hear["channel"]
 	tosay = something
 	return sc.apicall("chat.postMessage",
-						channel = channel,
-						text = tosay,
-						user = towho)		
+			  channel = channel,
+			  text = tosay,
+			  user = towho)		
 
 if hear.get("subtype") is None and "hi" in hear.get['text']:
 	greetings = "Hello <@%s>! :tada:" % hear["user"]
 	say(greetings)
 
-count = 0
-hand = True
-quantite = 0  # número de necesidades
+global turn, chicle. harvest # oui?
+turn = 0 # for the participants list.
+chicle = 0 # ciclo, numerito variable-
+harvest = 0 # 0 ~ 1, """ var arbitrario.
 
 
 def need(number):
@@ -58,22 +60,21 @@ def checking(ou, t):
 			# ..esta raro esta funcion, tenia sueño ya. LISTO :) '''
 
 
-seeds = { "need1" : 0,
-		  "need2" : 0,
-		  "need3" : 0}
+seeds = {"need1" : 0,
+	 "need2" : 0,
+	 "need3" : 0}
 
 class Seed:
-
-		def __init__(self, seed):
-			# its 'name', e. g. need1 = Seeds('')
-			'''The name will be 'need'+ str(number) <- it is need(), formely.'''
-      # EXAMPLE: 
-			# mot = message['text'] <- it's relation w/ hear= handle_message()
-      # needl = need()
-			# needl.add_mot('mot')
-			# then, if we print(need1.mot) -> ['', '', 'etc']
-			self.seed = seed
-			self.mots = []
+	def __init__(self, seed):
+		# its 'name', e. g. need1 = Seeds('')
+		'''The name will be 'need'+ str(number) <- it is need(), formely.'''
+      		# EXAMPLE: 
+		# mot = message['text'] <- it's relation w/ hear= handle_message()
+		# needl = need()
+		# needl.add_mot('mot')
+		# then, if we print(need1.mot) -> ['', '', 'etc']
+		self.seed = seed
+		self.mots = []
 
 		def add_mot(self, mot):
 			self.mots.append(mot)
@@ -83,53 +84,8 @@ class Seed:
 			self.submots.append(mot)
 
 
-def mot2harvest(nro):
-	#nro de 'cuenta'.
-  global nro  
-  hand = False
-  counter = 0
-	
-  while nro > 0: # it should start with 2 or 3, and decreasing.
-	  nro = - 1
-
-    needl = need(counter) # c'est le même qui 
-    say()
-  
-
-  say()# -->  the words added into the list add_mot() !
-
-
-  
-	return say()  #building it...
-
-
-def mot1harvest(quantite, cuenta = 0):
-  
-  planted = need(cuenta)
-  cuenta = + 1 
-
-  # planted.add_mot('input') ..it has to be in the listening.
-  say(print(seeds[planted])
-
-  if cuenta < quantite: # quantite = len(aux) <- Seeds.
-		return cuenta
-	else:
-		end(mot1harvest)
-
-		#time.sleep(60) # this function can't be.. 'cause we need Thanos keeps listening the words!
-
-    def timeSleep():
-      schedule.every().minute.do(schedule.clear('1st')).tag('1st') # making sure it'll do it just once.
-
-      say("2nd -")
-      return mot2harvest(cuenta) #nro, seeds) ..nope TODO.
-                      # ^ Still blding..
-
-		return schedule.every().minute.do(timeSleep()).tag('1st', 'harvest')
-
-
 def uP(): # getting ready!
-  userPrepare = ["start", "La siembra", "harvest", "la siembra", "sembrar"]
+	userPrepare = ["start", "La siembra", "harvest", "la siembra", "sembrar"]
 	for i in range(len(userPrepare) + 1):
 		up = userPrepare[i]
 		return up # user prepare
@@ -142,7 +98,7 @@ if hear.get("subtype") is None and uP() in hear.get['text']:
 
 
 def soul():
-  global users
+	global users
 	api_call = sc.api_call("users.list")
 	if api_call.get('ok'):
 		members = api_call.get('members')
@@ -150,56 +106,119 @@ def soul():
 			if 'name' in user and user.get('name') == (BOT_NAME or "Thanos"):
 				global BOT_ID
 				BOT_ID = '<@{}>'.format(user.get('id'))
-    users = api_call
+		users = api_call
 		return users
 
 
 participants = {principal, } '''  # (sets).
  done_TODO HEAR THE MESSAGE AND SAVE THE SEEDS/NEEDS into the dictionary "seeds".'''
 if hear.get("subtype") is None and (whispper == principal) and (len(participants) == 1):
-  banned = ['', " ", ",", "y", "and", "et"]
-  # ^ later we should create a properly file! (AI alike, huh).
-
-  ''' I've just realized that this next before decoration could be a
-  function, in fact this is "the SEEDING", so it'd be actually -def-
-  seeding():                    '''
-  for y in range(len(banned)):
-    aux = hear["text"]
-    
-    supre = banned[y]
-    if supre in aux:
-      aux.remove(supre)
-  for x in range(len(aux)):
-      d = Seed(aux[x])
-
-      seeds[need(x)] = d.seed  # dict[key] = value
-  # print(need(x)) -> prints that respective value !
-  # need(x).add_mot(WORD) # adds a mot into the [value] Seed class.
-  sayP = "Registrado, necesidades: " + print(seeds)
-  say(sayP, whispper)
+  	banned = ['', " ", ",", "y", "and", "et"]
+  	# ^ later we should create a properly file! (AI alike, huh).
+	
+  	''' I've just realized that this next before decoration could be a
+  	function, in fact this is "the SEEDING", so it'd be actually -def-
+  	seeding():                    '''
+	for y in range(len(banned)):
+		aux = hear["text"] 
+		
+    		supre = banned[y]
+    		if supre in aux:
+			aux.remove(supre)
+	for x in range(len(aux)):
+      		d = Seed(aux[x])
+		
+		seeds[need(x)] = d.seed  # dict[key] = value
+  	# print(need(x)) -> prints that respective value !
+  	# need(x).add_mot(WORD) # adds a mot into the [value] Seed class.
+  	sayP = "Registrado, necesidades: " + print(seeds)
+  	say(sayP, whispper)
 	
 	@soul()
-  # now, we start to ask people who wants to join the harvesting!
+  	# now, we start to ask people who wants to join the harvesting!
 	while True:
 
 		time.sleep(1)
 		if hear.get("subtype", whispper) is None and ("listo" or "ready") in hear.get["text"]:
-      participants = list(participants)
-      # I would like to print all participants. And if we are in Slack, Thanos as BOT should show it as an attachment.
-      # But well- as simple/easily to do so, first; It's necessary to have in mind the thing about be able in being 'compatible' w/ the next versions after completing the Slack BOT. (I mean, Telegram and whatsapp) But I know, let's get FOCUS!
+      
+      			# I would like to print all participants. And if we are in Slack, Thanos as BOT should show it as an attachment.
+      			# But well- as simple/easily to do so, first; It's necessary to have in mind the thing about be able in being 'compatible' w/ the next versions after completing the Slack BOT. (I mean, Telegram and whatsapp) But I know, let's get FOCUS!
       
 			break 
 		if hear.get("subtype") is None and ("yo" or "Yo" or "I" or "i" or ":raising_han:" or "me" or 'moi' or ) in hear.get['text']:
 			participants.add(whispper())
+	
 @say("Ok, let's start!")
+def scythe(mot, cual, donde, parte, chicle):
+	# word, harvest and needl. list^
+  	global A
+  	
+  	if cual == 0:
+    		donde.add_mot(mot)
+  	else: # cual = 1. It means 2nd harvest is on.
+    		chicle = + 1 # añade al contador, que estará dentro del while.
+    		#cuenta los ciclos, es decir que si es igual al nro de len()..
+    		donde.add_submot(mot)
+    	
+    	if turn == len(parte): # se iguala al número de participantes
+      		if chicle == len(donde.mots):
+        	A = + 1 # contador para la clase Seeds -> needs.
+        	donde = needl(A)
+        	chicle = 0      
+      	turn = 0
+      	this = donde.mots[chicle]
+      	say(this)
 
-start = mot1harvest(len(aux))
-schedule.every(60).seconds.do(mot1harvest(quantite=len(aux), start)))
+
+def harvesting():
+	if harvesting != 1 and is not chicle < len(aux):
+    		if chicle == len(aux):
+      			endgame(harvesting)
+      
+    		else:
+      			time.sleep(1)
+    		harvest = 1 # (2nd harvesting)
+    		chicle = 0
+  
+  	chicle = + 1  # cycle !
+  	needl = need(chicle - 1)
+
+  	if harvest != 1:
+    		say(print(seeds[needl]))
+
+  	parte = list(participants) 
+  	while True:
+	
+    		if turn > len(participants):
+      			turn = 0 # counts this list!
+      			parte = list(participants)
+      			# ^ this line rewrites the list.
+
+    		if harvest == 1 and chicle == len(needl.mots):
+      			chicle = 0 # counts this 
+      			needl = need()
+      			say(print(needl.mot))
+
+		if hear.get("subtype") is None:
+      			word = hear.get["text"] 
+      
+      			turn = + 1
+      			if whispper() != parte[turn]:
+        			try:
+          				damn = print(participant.pop(turn)) + " had talked. Let's continue.."
+          				if harvest != 1 or whispper() not in parte:
+          					turn = - 1  # se retribuye! Se compensa..
+        			except:
+          				damn = "be quite <@%s>" % whispper()
+          				pass
+        			finally:
+          				say(damn)
+      
+		scythe(word, harvest, needl, parte, chicle) # para que lo guarde.
+		#ahora necesito algo que lo diga y que 'salte'.
+			
+		
+schedule.every(60).seconds.do(harvesting())
+harvesting()
 
 
-if hear.get("subtype") is None and (whispper == )
-
-
-while hand:
-  if  count > 0:
-     mot2harvest(nro)
